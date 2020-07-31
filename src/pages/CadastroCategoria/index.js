79d7ad5227ab33c711e26a3b809fa57ae2d660e5
Ronan Luiz Araújo Eustáquio <ronanluiz@gmail.com>
2020-07-31 16:23:06 -0300

    Construção do formulário de cadastro de categorias

--- src/pages/CadastroCategoria/index.js
+++ src/pages/CadastroCategoria/index.js
@@ -1,11 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../components/PageDefault';
+import FormField from '../../components/FormField';
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categoriasCadastradas, setCategorias] = useState([]);
+  const [cadastro, setCadastro] = useState(valoresIniciais);
+
+  function setCampoCadastro(chave, valor) {
+    setCadastro({
+      ...cadastro,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(event) {
+    setCampoCadastro(
+      event.target.getAttribute('name'),
+      event.target.value,
+    );
+  }
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
+
+      <form onSubmit={function handleSubmit(infoFormulario) {
+        infoFormulario.preventDefault();
+
+        setCategorias([
+          ...categoriasCadastradas,
+          cadastro,
+        ]);
+
+        setCampoCadastro(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={cadastro.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={cadastro.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={cadastro.cor}
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+      </form>
+
+      <ul>
+        {categoriasCadastradas.map((categoria) => (
+          <li key={`${categoria.id}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
       <Link to="/">
         Ir para home
